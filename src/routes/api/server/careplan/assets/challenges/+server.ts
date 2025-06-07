4eb692119c2b8fbdc6afe04a825b37bf685bca1e
MadhuraAnarase <madhuraa712@gmail.com>
2025-06-07 11:12:44 +0530

    Added TenantId in Challenges

--- src/routes/api/server/careplan/assets/challenges/+server.ts
+++ src/routes/api/server/careplan/assets/challenges/+server.ts
@@ -33,6 +33,7 @@ export const POST = async (event: RequestEvent) => {
             data.Name,
             data.Description,     
             data.Tags,
+            data.TenantId,
             data.Version ?? '');
 
         return ResponseHandler.success(response);
