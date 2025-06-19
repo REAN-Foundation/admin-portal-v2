6c39acbddbf371360e5ddc080ec96fc16fc4faa0
MadhuraAnarase <madhuraa712@gmail.com>
2025-06-19 12:17:07 +0530

    Change Audio to Challenge in validation

--- src/lib/validation/challenges.schema.ts
+++ src/lib/validation/challenges.schema.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const createOrUpdateSchema = z.object({
     Name: z
         .string({
-            required_error: 'Audio name is required.',
-            invalid_type_error: 'Audio name must be a string.',
+            required_error: 'Challenge name is required.',
+            invalid_type_error: 'Challenge name must be a string.',
         })
-        .min(1, { message: 'Audio name cannot be empty.' })
-        .max(128, { message: 'Audio name must be at most 128 characters long.' }),
+        .min(1, { message: 'Challenge name cannot be empty.' })
+        .max(128, { message: 'Challenge name must be at most 128 characters long.' }),
 
     Description: z
         .string({
