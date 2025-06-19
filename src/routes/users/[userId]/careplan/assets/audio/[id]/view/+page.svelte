291f79a30b2f5a77fc33bab3dfd22bf8facb5d81
MadhuraAnarase <madhuraa712@gmail.com>
2025-06-19 11:47:44 +0530

    Audio view ui changed

--- src/routes/users/[userId]/careplan/assets/audio/[id]/view/+page.svelte
+++ src/routes/users/[userId]/careplan/assets/audio/[id]/view/+page.svelte
@@ -40,10 +40,10 @@
 
 <div class="mx-auto w-full px-6 py-4">
 	<div class="form-headers">
-		<h2 class="form-title">View Audio</h2>
+		<h2 class="form-titles">View Audio</h2>
 		<a href={assetRoute} class="cancel-btn">
-				<Icon icon="material-symbols:close-rounded" />
-			</a>
+			<Icon icon="material-symbols:close-rounded" />
+		</a>
 	</div>
 
 		<table class="w-full">
