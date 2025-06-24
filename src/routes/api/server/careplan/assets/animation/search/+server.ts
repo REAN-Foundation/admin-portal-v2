7edcf0d65f26b92011a5dbe9e00843e665a7d0c4
MadhuraAnarase <madhuraa712@gmail.com>
2025-06-24 12:49:14 +0530

    Removed commented code

--- src/routes/api/server/careplan/assets/animation/search/+server.ts
+++ src/routes/api/server/careplan/assets/animation/search/+server.ts
@@ -30,35 +30,3 @@ export const GET = async (event: RequestEvent) => {
 		return ResponseHandler.handleError(500, null, error);
 	}
 };
-
-
-// import type { RequestEvent } from '@sveltejs/kit';
-// import { ResponseHandler } from '$lib/utils/response.handler';
-// import { searchAnimation } from '../../../../../services/careplan/assets/animation';
-
-// //////////////////////////////////////////////////////////////
-
-// export const GET = async (event: RequestEvent) => {
-// 	try {
-// 		const sessionId = event.locals?.sessionUser?.sessionId;
-// 		if (!sessionId) {
-// 			return ResponseHandler.handleError(401, null, new Error('Access denied: Invalid session.'));
-// 		}
-
-// 		const searchParams: URLSearchParams = event.url.searchParams;
-// 		const searchFilters = {
-// 			name: searchParams.get('name') ?? 'undefined',
-// 			orderBy: searchParams.get('sortBy') ?? 'CreatedAt',
-// 			order: searchParams.get('sortOrder') ?? 'ascending',
-// 			itemsPerPage: parseInt(searchParams.get('itemsPerPage') ?? '10'),
-// 			pageIndex: parseInt(searchParams.get('pageIndex') ?? '0')
-// 		};
-
-// 		console.log('Search Parameters:', searchFilters);
-// 		const response = await searchAnimation(sessionId, searchFilters);
-// 		return ResponseHandler.success(response);
-// 	} catch (error) {
-// 		console.error('Error retrieving animation:', error);
-// 		return ResponseHandler.handleError(500, null, error);
-// 	}
-// };
