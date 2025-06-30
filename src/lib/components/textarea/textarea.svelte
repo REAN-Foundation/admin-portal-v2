723a047bcd0b17d8f3fed35b27a89a82fa39733e
mayur-bankar-inflection <mayur.s.bankar@inflectionzone.com>
2025-06-30 18:23:33 +0530

    event handling

--- src/lib/components/textarea/textarea.svelte
+++ src/lib/components/textarea/textarea.svelte
@@ -7,7 +7,16 @@
 		error = '',
 		className = '',
 		resize = false,
-		rows = 3
+		rows = 3,
+		// Events
+		oninput,
+		onchange,
+		onfocus,
+		onblur,
+		onkeydown,
+		onkeyup,
+		onpaste,
+		onselect
 	} = $props<{
 		name: string;
 		placeholder?: string;
@@ -16,6 +25,14 @@
 		className?: string;
 		resize?: boolean;
 		rows?: number;
+		oninput?: (e: Event) => void;
+		onchange?: (e: Event) => void;
+		onfocus?: (e: FocusEvent) => void;
+		onblur?: (e: FocusEvent) => void;
+		onkeydown?: (e: KeyboardEvent) => void;
+		onkeyup?: (e: KeyboardEvent) => void;
+		onpaste?: (e: ClipboardEvent) => void;
+		onselect?: (e: Event) => void;
 	}>();
 </script>
 
@@ -26,5 +43,13 @@
 		{rows}
 		{placeholder}
 		class={`input placeholder-gray-400 ${resize ? 'resize-y' : 'resize-none'} ${error ? 'border-error-300' : 'border-primary-200'} ${className}`}
+		{oninput}
+		{onchange}
+		{onfocus}
+		{onblur}
+		{onkeydown}
+		{onkeyup}
+		{onpaste}
+		{onselect}
 	></textarea>
 </div>
