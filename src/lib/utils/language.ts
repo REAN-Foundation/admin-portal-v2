d2cea6439e05c90882fc7da9dfad3d25e67402cf
Dattatraya-Londhe <londhedn@gmail.com>
2025-12-09 10:20:53 +0000

    Refactor language list to include additional languages

--- src/lib/utils/language.ts
+++ src/lib/utils/language.ts
@@ -1,13 +1,12 @@
 // src/lib/constants/languages.ts
 
 export const languages = [
-	{ name: 'English', code: 'en' },
-	{name: 'Marathi', code:'mr'},
-	{ name: 'Hindi', code: 'hi' },
-	// { name: 'French', code: 'fr' },
-	// { name: 'German', code: 'de' },
-	// { name: 'Arabic', code: 'ar' },
-	// { name: 'Chinese', code: 'zh' },
-	// { name: 'Japanese', code: 'ja' },
-	// { name: 'Korean', code: 'ko' }
+  { name: 'English', code: 'en' },
+  { name: 'Marathi', code: 'mr' },
+  { name: 'Hindi', code: 'hi' },
+  { name: 'Igbo', code: 'ig' },
+  { name: 'Hausa', code: 'ha' },
+  { name: 'Swahili', code: 'sw' },
+  { name: 'Yoruba', code: 'yo' },
+  { name: 'Punjabi', code: 'pa' },
 ];
