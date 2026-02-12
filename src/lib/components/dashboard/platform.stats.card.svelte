0a6b596cbb388661efbdbb62b05184f9efd5df08
rupali-inflection <rupalidinde@gmail.com>
2026-02-12 11:24:37 +0530

    Fixed the null data for the home screen

--- src/lib/components/dashboard/platform.stats.card.svelte
+++ src/lib/components/dashboard/platform.stats.card.svelte
@@ -8,19 +8,26 @@
 	const iOSUsers: any = [];
 	const years: any = [];
 
-	for (let i = 0; i < deviceDetails.length; i++) {
-		totalUsers.push(deviceDetails[i].UserCount);
-		years.push(deviceDetails[i].Year);
-		androidUsers.push(deviceDetails[i].DeviceDetails[0].Count);
-		iOSUsers.push(deviceDetails[i].DeviceDetails[1].Count);
+	const details = Array.isArray(deviceDetails) ? deviceDetails : [];
+	for (let i = 0; i < details.length; i++) {
+		totalUsers.push(details[i]?.UserCount ?? 0);
+		years.push(details[i]?.Year ?? '');
+		androidUsers.push(details[i]?.DeviceDetails?.[0]?.Count ?? 0);
+		iOSUsers.push(details[i]?.DeviceDetails?.[1]?.Count ?? 0);
 	}
 </script>
 
 <div class="status-card h-auto w-auto sm:w-full sm:p-6">
 	<div class="w-full pb-8">
 		<h4 class="sub-heading mb-5 justify-center text-center">Platform Based Users</h4>
-		<div class="h-auto w-full">
-			<PlatformBasedChart {totalUsers} {androidUsers} {iOSUsers} lables={years} />
-		</div>
+		{#if details.length > 0}
+			<div class="h-auto w-full">
+				<PlatformBasedChart {totalUsers} {androidUsers} {iOSUsers} lables={years} />
+			</div>
+		{:else}
+			<div class="flex items-center justify-center py-10">
+				<p class="text-gray-500">Data not available</p>
+			</div>
+		{/if}
 	</div>
 </div>
