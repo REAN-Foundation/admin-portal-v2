a93ce181fe4b8bed744486a783cce15d0b4b3034
rupali-inflection <rupalidinde@gmail.com>
2026-02-25 10:52:51 +0530

    Css fixes

--- src/lib/components/analytics/EmptyState.svelte
+++ src/lib/components/analytics/EmptyState.svelte
@@ -2,6 +2,6 @@
 	let { message = 'Data Not Available' } = $props();
 </script>
 
-<div class="flex h-64 w-full items-center justify-center rounded-lg border border-[var(--color-outline)] shadow-lg">
+<div class="flex h-64 w-full items-center my-4 justify-center rounded-lg border border-[var(--color-outline)] shadow-lg">
 	<p class="text-center text-lg text-[var(--color-info)]">{message}</p>
 </div>
