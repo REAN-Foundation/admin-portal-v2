9c0c05ab445b904156f968d5f3069a37407efe53
rupali-inflection <rupalidinde@gmail.com>
2026-03-17 14:18:53 +0530

    Added search filter for the enrollment

--- src/routes/api/server/careplan/enrollments/search/+server.ts
+++ src/routes/api/server/careplan/enrollments/search/+server.ts
@@ -1,4 +1,5 @@
 import { ResponseHandler } from "$lib/utils/response.handler";
+import { createSearchFilters } from "$lib/utils/search.utils";
 import { searchEnrollments } from "$routes/api/services/careplan/enrollments";
 import type { RequestEvent } from "@sveltejs/kit";
 
@@ -11,19 +12,14 @@ export const GET = async (event: RequestEvent) => {
             return ResponseHandler.handleError(401, null, new Error("Access denied: Invalid session."));
         }
 
-        const searchParams: URLSearchParams = event.url.searchParams;
-        const searchFilters = {
-            careplanId: searchParams.get('carePlan') ?? undefined,
-            displayId: searchParams.get("displayId") ?? undefined,
-            startDate: searchParams.get("startDate") ?? undefined,
-            endDate: searchParams.get("endDate") ?? undefined,
-            orderBy: searchParams.get("sortBy") ?? "CreatedAt",
-            order: searchParams.get("sortOrder") ?? "ascending",
-            itemsPerPage: parseInt(searchParams.get("itemsPerPage") ?? "10"),
-            pageIndex: parseInt(searchParams.get("pageIndex") ?? "0"),
-        };
+        const searchFilters = createSearchFilters(event, {
+            // useTenantCode: true,
+            careplanId: event.url.searchParams.get('carePlan') ?? undefined,
+            displayId: event.url.searchParams.get("displayId") ?? undefined,
+            startDate: event.url.searchParams.get("startDate") ?? undefined,
+            endDate: event.url.searchParams.get("endDate") ?? undefined,
+        });
 
-        console.log("Search Parameters--------:", searchFilters);
         const response = await searchEnrollments(sessionId, searchFilters);
         return ResponseHandler.success(response);
 
